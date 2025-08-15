d28d86fa2c781ba55606285af65f982ca9550379
dtucker <davidt2003@msn.com>
2025-08-15 16:50:48 -0400

    feat: prepare template JS script for forms

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,8 +1,27 @@
+import Form from "./pulsar/modules/form.js";
+
 export default class Application {
 
+    #formEngine;
     #configurations;
+    i18n;
 
     constructor(configurations) {
         this.#configurations = configurations;
+        this.i18n = configurations.i18n;
+    }
+
+    initialize() {
+        this.#initializeForms();
+    }
+
+    getFormEngine() {
+        return this.#formEngine;
+    }
+
+    #initializeForms() {
+        this.#formEngine = new Form(this.i18n.buttons.loading);
+        this.#formEngine.preventDoubleSubmission();
+        this.#formEngine.autoResizeTextarea();
     }
 }
