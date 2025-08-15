67c1a49390610936ee9c9d310743cf3006c523d6
dtucker <davidt2003@msn.com>
2025-08-15 16:40:53 -0400

    feat: add Now entity

---
+++ app/Models/Core/Entities/Now.php
@@ -0,0 +1,22 @@
+<?php namespace Models\Core\Entities;
+
+// TODO: Move to Zephyrus ?
+class Now
+{
+    public string $year;
+    public string $month;
+    public string $day;
+    public string $date_time;
+    public string $date;
+    public string $time;
+
+    public function __construct()
+    {
+        $this->year = date('Y');
+        $this->month = date('m');
+        $this->day = date('d');
+        $this->date_time = date(FORMAT_DATE_TIME);
+        $this->date = date(FORMAT_DATE);
+        $this->time = date(FORMAT_TIME);
+    }
+}
